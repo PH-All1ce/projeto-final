72fe36460793585674bc735eb38cdba868796581
H3R0FDJR <douglas.frt17@gmail.com>
2025-12-03 19:35:49 -0300

    erro de formatação ao enviar para o git

--- core/templatetags/url_extras.py
+++ core/templatetags/url_extras.py
@@ -2,12 +2,13 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def remove_page(url_string):
     """
     Remove o parâmetro 'page' de uma string de parâmetros de URL (query string).
     Isso é crucial para manter os filtros ao paginar.
     """
-    params = url_string.split('&')
-    filtered_params = [p for p in params if not p.startswith('page=')]
-    return '&'.join(filtered_params)
+    params = url_string.split("&")
+    filtered_params = [p for p in params if not p.startswith("page=")]
+    return "&".join(filtered_params)
